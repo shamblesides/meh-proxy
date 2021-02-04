007a4f6d0ad74cf5e4384d7253cf489e5703333e
Nigel Nelson <nigel.mark.nelson@gmail.com>
2021-02-03 20:15:28 -0700

    logging

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const httpProxy = require('http-proxy')
 
 const proxy = httpProxy.createProxyServer();
 
+function log(request, ...msg) {
+  const ipHeader = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+  const ip = ipHeader.split(',').pop().trim();
+  console.log(new Date().toISOString(), ip, request.url.substr(0, 30).padEnd(30, ' '), ...msg)
+}
+
 const httpServer = http.createServer(function(request, response) {
   proxy.web(request, response, { target: process.env.URL, secure: false, changeOrigin: true }, (err) => {
-    console.log(`request error: ${err.message}`)
+    log(request, `request error: ${err.message}`)
   });
+  log(request)
 });
 
 httpServer.on('upgrade', function upgrade(request, socket, head) {
   proxy.ws(request, socket, head, { target: process.env.URL, secure: false, changeOrigin: true }, (err) => {
-    console.error(`socket upgrade error: ${err.message}`)
+    log(request, `socket upgrade error: ${err.message}`)
   })
+  log(request);
 })
 
 const listener = httpServer.listen(process.env.PORT, (err) => {
   if (err) {
-    console.error(`Failed to start: ${err}`);
+    console.error(new Date().toISOString(), `Failed to start: ${err}`);
     process.exit(1);
   } else {
-    console.log(`Proxy listening on port ${listener.address().port}`)
+    console.log(new Date().toISOString(), `Proxy listening on port ${listener.address().port}`)
   }
 })
